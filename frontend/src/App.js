eac4c70c040b0d48f36b9a051d2972abed28a1fc
AJStanchfield <ajstanchfield@gmail.com>
2025-12-02 00:01:15 -0500

    created reactapp

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/hello/")
+      .then((res) => res.json())
+      .then((data) => setMsg(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Django</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
+
