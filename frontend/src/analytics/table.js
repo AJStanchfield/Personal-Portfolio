97bfdd6c3a7ff0ebf465230db6021919f56e9af3
AJ <ajstanchfield@gmail.com>
2025-12-19 15:04:35 -0500

    centered title

--- frontend/src/analytics/table.js
+++ frontend/src/analytics/table.js
@@ -54,95 +54,85 @@ const formatPercent = (num) => {
 
     return (
         <>
-            <div style={{ marginBottom: "75px" }}>
-                <div style={{ display: "flex", float: "left" }}>
-                </div>
-                <h1 style={{
-                        align: "center",
-                        color: "#d850c3",
-                        background: "white",
-                        borderRadius: "3px",
-                        display: "center",
-                        minWidth: "300px",
-                        textAlign: "center"}}>Analytics Dashboard</h1>
-
-
+        <div className="section-content" style={{ width: '100%', maxWidth: 1100, margin: '0 auto', padding: '20px' }}>
+            <h1 style={{ textAlign: 'center', margin: '0 0 16px 0' }}>Analytics Dashboard</h1>
+            <TableContainer
+                className="analytics-table four-row mt-3 Financial"
+                component={Paper}
+                style={{ width: '100%' }}>
+                <Table sx={{ minWidth: 650 }}>
+                        {getTableHead("Financials")}
+                        <TableBody>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Total Sales
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Total Sales" /></TableCell>
+                            </TableRow>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Route Sales
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Route Sales" /></TableCell>
+                            </TableRow>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Event Sales
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Event Sales" /></TableCell>
+                            </TableRow>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Average Order Value
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Average Order Value" /></TableCell>
+                            </TableRow>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Refunds
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Refunds" /></TableCell>
+                            </TableRow>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Discounts
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Discounts" /></TableCell>
+                            </TableRow>
+                            <TableRow hover={true}>
+                                <TableCell>
+                                    Tips
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="center"></TableCell>
+                                <TableCell align="right"><ChartButton DataName="Tips" /></TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
-                <>
-                    <TableContainer className="analytics-table four-row mt-3 Financial" component={Paper}>
-                        <Table sx={{ minWidth: 650 }}>
-                            {getTableHead("Financials")}
-                            <TableBody>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Total Sales
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Total Sales"/></TableCell>
-                                </TableRow>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Route Sales
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Route Sales"/></TableCell>
-                                </TableRow>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Event Sales
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Event Sales"/></TableCell>
-                                </TableRow>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Average Order Value
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Average Order Value"/></TableCell>
-                                </TableRow>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Refunds
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Refunds"/></TableCell>
-                                </TableRow>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Discounts
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Discounts"/></TableCell>
-                                </TableRow>
-                                <TableRow hover={true}>
-                                    <TableCell>
-                                        Tips
-                                    </TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="center"></TableCell>
-                                    <TableCell align="right"><ChartButton DataName="Tips"/></TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+        </>
 
 
-                </>
-            </>
     );
 }
 
